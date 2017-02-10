fec2e7e7eb576f8921d5212c8880994064207f3a
Rush3D <vanesdemon96@gmail.com>
2017-02-10 21:09:46 +0700

    Инкапсуляция

--- js/form.js
+++ js/form.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var form = document.querySelector('.notice__form');
-var formTitle = form.querySelector('#title');
-var formPrice = form.querySelector('#price');
-var formAddress = form.querySelector('#address');
-var formTime = form.querySelector('#time');
-var formTimeout = form.querySelector('#timeout');
-var formType = form.querySelector('#type');
-var formRoomNumber = form.querySelector('#room_number');
-var formCapacity = form.querySelector('#capacity');
+(function () {
+  var form = document.querySelector('.notice__form');
+  var formTitle = form.querySelector('#title');
+  var formPrice = form.querySelector('#price');
+  var formAddress = form.querySelector('#address');
+  var formTime = form.querySelector('#time');
+  var formTimeout = form.querySelector('#timeout');
+  var formType = form.querySelector('#type');
+  var formRoomNumber = form.querySelector('#room_number');
+  var formCapacity = form.querySelector('#capacity');
 
-formPrice.required = true;
-formPrice.max = 1000000;
-formTitle.required = true;
-formTitle.minLength = 30;
-formTitle.maxLength = 100;
-formAddress.required = true;
+  formPrice.required = true;
+  formPrice.max = 1000000;
+  formTitle.required = true;
+  formTitle.minLength = 30;
+  formTitle.maxLength = 100;
+  formAddress.required = true;
 
-window.synchronizeFields(formTime, formTimeout, ['12', '13', '14'], ['12', '13', '14'], 'value');
-window.synchronizeFields(formType, formPrice, ['flat', 'bungalow', 'palace'], ['1000', '0', '10000'], 'min');
-window.synchronizeFields(formRoomNumber, formCapacity, ['1', '2', '100'], ['0', '3', '3'], 'value');
-
-window.initializePins();
+  window.synchronizeFields(formTime, formTimeout, ['12', '13', '14'], ['12', '13', '14'], 'value');
+  window.synchronizeFields(formType, formPrice, ['flat', 'bungalow', 'palace'], ['1000', '0', '10000'], 'min');
+  window.synchronizeFields(formRoomNumber, formCapacity, ['1', '2', '100'], ['0', '3', '3'], 'value');
+})();
--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,103 +1,112 @@
 'use strict';
 
-window.initializePins = function () {
-  var pinMap = document.querySelector('.tokyo__pin-map');
-  var dialog = document.querySelector('.dialog');
-  var dialogClose = dialog.querySelector('.dialog__close');
-  var ENTER_KEY_CODE = 13;
-  var ESCAPE_KEY_CODE = 27;
+window.initializePins = (function () {
+  (function () {
+    var pinMap = document.querySelector('.tokyo__pin-map');
+    var dialog = document.querySelector('.dialog');
+    var dialogClose = dialog.querySelector('.dialog__close');
 
-  pinMap.addEventListener('keydown', function (e) {
-    if (isActivateEvent(e)) {
-      eventHandlerPin(e);
-    }
-  });
+    var ESCAPE_KEY_CODE = 27;
 
-  pinMap.addEventListener('click', eventHandlerPin);
+    pinMap.addEventListener('keydown', function (e) {
+      if (window.utils.isActivateEvent(e)) {
+        eventHandlerPin(e);
+      }
+    });
 
-  dialogClose.addEventListener('click', function (e) {
-    e.preventDefault();
-    hideDialog();
-    removeActivePin();
-  });
+    pinMap.addEventListener('click', eventHandlerPin);
 
-  /**
-   * Функция-обработчик события клика или нажатия с клавиатуры
-   * по одному из пинов (маркеры)
-   * @param {MouseEvent} e Событие
-   */
-  function eventHandlerPin(e) {
-    var pin = (e.target.classList.contains('pin')) ? e.target : e.target.parentElement;
-    removeActivePin();
-    addActivePin(pin);
-    showDialog();
-  }
-
-  /**
-   * На нажатый маркер добавляется класс активности
-   * @param {HTMLDivElement} pin Нажатый маркер
-   */
-  function addActivePin(pin) {
-    pin.classList.add('pin--active');
-    toggleAriaPressed(pin);
-  }
-
-  /**
-   * Ищем маркер, который активен в данный момент
-   * Если таковой имеется, то деактивируем его
-   */
-  function removeActivePin() {
-    var pinActive = document.querySelector('.pin--active');
-    if (pinActive) {
-      pinActive.classList.remove('pin--active');
-      toggleAriaPressed(pinActive);
-    }
-  }
-  /**
-   * Проверяем нажатие Enter
-   * @param {KeyboardEvent} e
-   * @return {Boolean} Возвращается true, если был нажат Enter
-   */
-  function isActivateEvent(e) {
-    return e.keyCode === ENTER_KEY_CODE;
-  }
-
-  /**
-   * Проверяем был ли нажат Escape
-   * Если был, то скрываем окно диалога и деактивируем активный пин
-   * @param {KeyboardEvent} e
-   */
-  function eventHandlerKeydownDialog(e) {
-    if (e.keyCode === ESCAPE_KEY_CODE) {
-      removeActivePin();
+    dialogClose.addEventListener('click', function (e) {
+      e.preventDefault();
       hideDialog();
+      removeActivePin();
+    });
+
+    /**
+     * Функция-обработчик события клика или нажатия с клавиатуры
+     * по одному из пинов (маркеры)
+     * @param {MouseEvent} e Событие
+     */
+    function eventHandlerPin(e) {
+      var pin = (e.target.classList.contains('pin')) ? e.target : e.target.parentElement;
+      removeActivePin();
+      addActivePin(pin);
+      showDialog();
     }
-  }
 
-  /**
-   * Показываем окно диалога и добавляем
-   * прослушку события нажатия на клавиатуру
-   */
-  function showDialog() {
-    dialog.style.display = 'block';
-    document.addEventListener('keydown', eventHandlerKeydownDialog);
-  }
+    /**
+     * На нажатый маркер добавляется класс активности
+     * @param {HTMLDivElement} pin Нажатый маркер
+     */
+    function addActivePin(pin) {
+      pin.classList.add('pin--active');
+      window.utils.toggleAriaPressed(pin);
+    }
 
-  /**
-   * Скрываем окно диалога и удаляем
-   * прослушку события нажатия на клавиатуру
-   */
-  function hideDialog() {
-    dialog.style.display = 'none';
-    document.removeEventListener('keydown', eventHandlerKeydownDialog);
-  }
+    /**
+     * Ищем маркер, который активен в данный момент
+     * Если таковой имеется, то деактивируем его
+     */
+    function removeActivePin() {
+      var pinActive = document.querySelector('.pin--active');
+      if (pinActive) {
+        pinActive.classList.remove('pin--active');
+        window.utils.toggleAriaPressed(pinActive);
+      }
+    }
 
-  /**
-   * Переключаем состояние роли Button 'aria-pressed'
-   * @param {HTMLDivElement} element
-   */
-  function toggleAriaPressed(element) {
-    var pressed = (element.getAttribute('aria-pressed') === 'true');
-    element.setAttribute('aria-pressed', !pressed);
-  }
-};
+    /**
+     * Проверяем был ли нажат Escape
+     * Если был, то скрываем окно диалога и деактивируем активный пин
+     * @param {KeyboardEvent} e
+     */
+    function eventHandlerKeydownDialog(e) {
+      if (e.keyCode === ESCAPE_KEY_CODE) {
+        removeActivePin();
+        hideDialog();
+      }
+    }
+
+    /**
+     * Показываем окно диалога и добавляем
+     * прослушку события нажатия на клавиатуру
+     */
+    function showDialog() {
+      dialog.style.display = 'block';
+      document.addEventListener('keydown', eventHandlerKeydownDialog);
+    }
+
+    /**
+     * Скрываем окно диалога и удаляем
+     * прослушку события нажатия на клавиатуру
+     */
+    function hideDialog() {
+      dialog.style.display = 'none';
+      document.removeEventListener('keydown', eventHandlerKeydownDialog);
+    }
+  })();
+
+  window.utils = (function () {
+    var ENTER_KEY_CODE = 13;
+
+    return {
+      /**
+       * Переключаем состояние роли Button 'aria-pressed'
+       * @param {HTMLDivElement} element
+       */
+      toggleAriaPressed: function (element) {
+        var pressed = (element.getAttribute('aria-pressed') === 'true');
+        element.setAttribute('aria-pressed', !pressed);
+      },
+
+      /**
+       * Проверяем нажатие Enter
+       * @param {KeyboardEvent} e
+       * @return {Boolean} Возвращается true, если был нажат Enter
+       */
+      isActivateEvent: function (e) {
+        return e.keyCode === ENTER_KEY_CODE;
+      }
+    };
+  })();
+})();
