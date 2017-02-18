31229e58c748a5240daf2fdbdb6b9016e5825b9f
Rush3D <vanesdemon96@gmail.com>
2017-02-18 20:30:04 +0700

    Внесение правок

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -5,28 +5,49 @@ window.initializePins = (function () {
 
   pinMap.addEventListener('keydown', function (e) {
     if (window.utils.isActivateEvent(e)) {
-      eventHandlerPin(e);
+      eventHandlerKeydownPin(e);
     }
   });
 
-  pinMap.addEventListener('click', eventHandlerPin);
+  pinMap.addEventListener('click', eventHandlerClickPin);
 
   /**
-   * Функция-обработчик события клика или нажатия с клавиатуры
-   * по одному из пинов (маркеры)
-   * @param {MouseEvent|KeyboardEvent} e Событие
+   * Функция-обработчик события клика по одному из пинов.
+   * Функция вызывает showCard для открытия диалога вместе с коллбэком,
+   * который уберет активный пин в случае закрытия диалога
+   * @param {MouseEvent} e Нажатие мышкой
    */
-  function eventHandlerPin(e) {
+  function eventHandlerClickPin(e) {
     var pin = (e.target.classList.contains('pin')) ? e.target : e.target.parentElement;
+
     removeActivePin();
     addActivePin(pin);
-    var changePinProperties = (e.type === 'keydown') ? function () {
-      pin.focus();
-      removeActivePin();
-    } : function () {
-      removeActivePin();
-    };
-    window.showCard(changePinProperties);
+
+    window.showCard(removeActivePin);
+  }
+
+  /**
+   * Функция-обработчик события нажатия Enter по одному из пинов.
+   * Функция вызывает showCard для открытия диалога вместе с коллбэком,
+   * который уберет активный пин и вернет на него фокус в случае закрытия диалога
+   * @param {KeyboardEvent} e Нажатие с клавиатуры
+   */
+  function eventHandlerKeydownPin(e) {
+    removeActivePin();
+    addActivePin(e.target);
+
+    window.showCard(function () {
+      returnFocusPin(e.target);
+    });
+  }
+
+  /**
+   * Функция удаляет активный маркер и возвращает на него фокус
+   * @param {HTMLDivElement} pin Маркер, которому необходимо вернуть фокус при закрытии диалога
+   */
+  function returnFocusPin(pin) {
+    removeActivePin();
+    pin.focus();
   }
 
   /**
@@ -44,6 +65,7 @@ window.initializePins = (function () {
    */
   function removeActivePin() {
     var pinActive = document.querySelector('.pin--active');
+
     if (pinActive) {
       pinActive.classList.remove('pin--active');
       pinActive.setAttribute('aria-pressed', 'false');
